60228f8a253d2e8a32103ab362853789da479bb7
Hoki <kunihito.hoki@gmail.com>
2019-05-09 22:24:23 +0900

    add data/posi10000.txt.xz

---
+++ bin/dbg-net.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo Usage: `basename $0` positions weight
+    exit
+fi
+
+fposi=$1
+if [ ! -f $fposi ]
+then
+    echo cannot find $fposi
+    exit
+fi
+
+fwght=$2
+if [ ! -f $fwght ]
+then
+    echo cannot find $fwght
+    exit
+fi
+
+while read line
+do
+    echo $line
+    echo dbgout
+done < <(xz -dc $fposi) > >(bin/aobaz -q -w $fwght)
